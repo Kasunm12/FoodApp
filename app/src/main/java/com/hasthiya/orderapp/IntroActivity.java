f10897463c0ae54d505874b7dd6602d09a58de67
Kasun M.Weerasekara <98303334+Kasunm12@users.noreply.github.com>
2023-01-18 02:18:57 +0530

    Done the search main task

---
+++ app/src/main/java/com/hasthiya/orderapp/IntroActivity.java
@@ -0,0 +1,32 @@
+package com.hasthiya.orderapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class IntroActivity extends AppCompatActivity {
+    private ConstraintLayout startBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        getSupportActionBar().hide(); //hide action bar
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        startBtn=findViewById(R.id.startBtn);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(IntroActivity.this,MainActivity.class));
+            }
+        });
+    }
+}
